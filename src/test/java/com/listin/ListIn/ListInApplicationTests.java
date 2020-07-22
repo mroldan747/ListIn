76ef3073d8c682c74ecf5f8f46de0a655ae179c4
mroldan747 <mroldan747@gmail.com>
2020-07-22 11:19:35 +0200

    first commit

---
+++ src/test/java/com/listin/ListIn/ListInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listin.ListIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
